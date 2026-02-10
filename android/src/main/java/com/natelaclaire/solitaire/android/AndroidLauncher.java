e2e8b18d02a1e4029e13c9192e7fcffdcc865f84
Nate LaClaire <nate@natelaclaire.com>
2026-02-09 22:16:10 -0500

    Initial commit

---
+++ android/src/main/java/com/natelaclaire/solitaire/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.natelaclaire.solitaire.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.natelaclaire.solitaire.SolitaireGame;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new SolitaireGame(), configuration);
+    }
+}
